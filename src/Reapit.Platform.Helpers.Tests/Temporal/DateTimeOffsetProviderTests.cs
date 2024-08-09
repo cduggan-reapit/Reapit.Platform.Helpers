f67cd94a662a7cb3d8bac6cc4bf2c6095cfc9e3e
cduggan <cduggan@reapit.com>
2024-08-09 14:10:07 +0100

    feat: add simple datetimeoffset provider

---
+++ src/Reapit.Platform.Helpers.Tests/Temporal/DateTimeOffsetProviderTests.cs
@@ -0,0 +1,20 @@
+﻿using Reapit.Platform.Helpers.Temporal;
+
+namespace Reapit.Platform.Helpers.Tests.Temporal;
+
+public class DateTimeOffsetProviderTests
+{
+    [Fact]
+    public void Now_ReturnsCurrentTime_WhenNoContextConfigured()
+    {
+        DateTimeOffsetProvider.Now.Should().BeCloseTo(DateTimeOffset.Now, TimeSpan.FromMilliseconds(1));
+    }
+    
+    [Fact]
+    public void Now_ReturnsConfiguredTime_WhenContextConfigured()
+    {
+        var fixedTimestamp = new DateTimeOffset(2016, 4, 16, 7, 53, 14, TimeSpan.FromHours(-5));
+        using var ambientContext = new DateTimeOffsetProviderContext(fixedTimestamp);
+        DateTimeOffsetProvider.Now.Should().Be(fixedTimestamp);
+    }   
+}
---
+++ src/Reapit.Platform.Helpers/Temporal/DateTimeOffsetProviderContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace Reapit.Platform.Helpers.Temporal;
+
+/// <summary>Execution context for the <see cref="DateTimeOffsetProvider"/> class</summary>
+public class DateTimeOffsetProviderContext : IDisposable
+{
+    internal DateTimeOffset Timestamp;
+    private static readonly ThreadLocal<Stack> ThreadScopeStack = new (() => new Stack());
+    private Stack _contextStack = new ();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DateTimeOffsetProviderContext"/> class
+    /// </summary>
+    /// <param name="timestamp"></param>
+    public DateTimeOffsetProviderContext(DateTimeOffset timestamp)
+    {
+        Timestamp = timestamp;
+        ThreadScopeStack.Value?.Push(this);
+    }
+
+    /// <summary>
+    /// The timestamp configured for the current execution context
+    /// </summary>
+    public static DateTimeOffsetProviderContext? Current
+    {
+        get
+        {
+            if ((ThreadScopeStack.Value?.Count ?? 0) == 0)
+                return null;
+            
+            return ThreadScopeStack.Value?.Peek() as DateTimeOffsetProviderContext;
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        ThreadScopeStack.Value?.Pop();
+        GC.SuppressFinalize(this);
+    }
+}
